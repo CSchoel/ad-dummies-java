31ebc035ccb1dd93486fc2354197739fd55df03d
Christopher Schölzel <christopher.schoelzel@gmx.net>
2019-08-01 23:15:40 +0200

    return zero as default

--- src/main/java/ad/dummies/p01basics/c01algorithms/E02GGT.java
+++ src/main/java/ad/dummies/p01basics/c01algorithms/E02GGT.java
@@ -6,8 +6,8 @@ public class E02GGT {
     public static int ggt2(int n, int m) {
         if (n == 0) { return m; }
         if (m == 0) { return n; }
-        return IntStream.range(2, Math.min(n, m))
-                .filter(x -> n % x == 0 && m % x == 0).max().getAsInt();
+        return IntStream.range(2, Math.min(n, m) + 1)
+                .filter(x -> n % x == 0 && m % x == 0).max().orElse(0);
     }
 
     public static int ggt3(int n, int m) {
