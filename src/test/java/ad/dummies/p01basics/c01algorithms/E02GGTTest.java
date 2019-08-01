d5150b17f67a1e5bdc1246ef1d072f3383ef824a
Christopher Schölzel <christopher.schoelzel@gmx.net>
2019-08-01 23:23:04 +0200

    adds more ggt tests

--- src/test/java/ad/dummies/p01basics/c01algorithms/E02GGTTest.java
+++ src/test/java/ad/dummies/p01basics/c01algorithms/E02GGTTest.java
@@ -15,6 +15,10 @@ public class E02GGTTest {
         assertEquals(1, ggt.apply(0, 1));
         assertEquals(2, ggt.apply(4, 2));
         assertEquals(3, ggt.apply(3 * 7 * 13, 3 * 3 * 11));
+        assertEquals(55, ggt.apply(5 * 11 * 23, 5 * 11 * 2 * 2));
+        assertEquals(1, ggt.apply(3 * 7 * 11, 5 * 13 * 17));
+        assertEquals(1024, ggt.apply(1024, 1024 * 13));
+        assertEquals(1, ggt.apply((int) Short.MAX_VALUE, Short.MAX_VALUE - 1));
     }
 
     @Test
