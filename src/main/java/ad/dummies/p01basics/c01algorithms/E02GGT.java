52a102121d27cd34aac8f62b98b94f66887acfd0
Christopher Schölzel <christopher.schoelzel@gmx.net>
2019-08-01 23:14:35 +0200

    bugfix: handle zero inputs

--- src/main/java/ad/dummies/p01basics/c01algorithms/E02GGT.java
+++ src/main/java/ad/dummies/p01basics/c01algorithms/E02GGT.java
@@ -4,11 +4,15 @@ import java.util.stream.IntStream;
 
 public class E02GGT {
     public static int ggt2(int n, int m) {
+        if (n == 0) { return m; }
+        if (m == 0) { return n; }
         return IntStream.range(2, Math.min(n, m))
                 .filter(x -> n % x == 0 && m % x == 0).max().getAsInt();
     }
 
     public static int ggt3(int n, int m) {
+        if (n == 0) { return m; }
+        if (m == 0) { return n; }
         int t = 1;
         for(int x = 2; x <= Math.min(n, m); x++) {
             if(isDivisor(x, m) && isDivisor(x, n)) {
