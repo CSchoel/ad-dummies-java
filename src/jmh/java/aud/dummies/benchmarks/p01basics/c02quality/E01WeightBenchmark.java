875e4239e3452d106ef7c3db80b2b3d599cd8ae8
Christopher Schölzel <christopher.schoelzel@gmx.net>
2019-08-24 11:40:24 +0200

    updates weight benchmark with state

--- src/jmh/java/aud/dummies/benchmarks/p01basics/c02quality/E01WeightBenchmark.java
+++ src/jmh/java/aud/dummies/benchmarks/p01basics/c02quality/E01WeightBenchmark.java
@@ -9,12 +9,30 @@ import java.util.concurrent.TimeUnit;
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
 @Fork(1) // execute each benchmark on it's own JVM
-@Warmup(iterations = 5, time = 2)
-@Measurement(iterations = 10, time = 2) // how many measurement iterations?
+@Warmup(iterations = 4, time = 1)
+@Measurement(iterations = 5, time = 1) // how many measurement iterations?
 public class E01WeightBenchmark {
+
+    @State(Scope.Thread)
+    public static class WeightSetup {
+        @Param({"10", "100", "1000", "10000", "100000"})
+        private int length;
+        private double[] weights;
+
+        @Setup
+        public void setup() {
+            // TODO: proper worst case
+            weights = new Random().doubles().limit(length).toArray();
+        }
+    }
+
     @Benchmark
-    public int benchmarkWeight1() {
-        double[] weights = new Random().doubles().limit(10000).toArray();
-        return E01Weight.minWeight1(weights);
+    public int benchmarkWeight1(WeightSetup state) {
+        return E01Weight.minWeight1(state.weights);
+    }
+
+    @Benchmark
+    public int benchmarkWeight2(WeightSetup state) {
+        return E01Weight.minWeight2(state.weights);
     }
 }
