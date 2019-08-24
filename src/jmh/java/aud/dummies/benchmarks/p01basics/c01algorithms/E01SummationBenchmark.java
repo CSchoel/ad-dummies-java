d251caa94cb1ff712e32e68eaa0326dcbef1441b
Christopher Schölzel <christopher.schoelzel@gmx.net>
2019-08-24 11:40:34 +0200

    adds summation benchmark

---
+++ src/jmh/java/aud/dummies/benchmarks/p01basics/c01algorithms/E01SummationBenchmark.java
@@ -0,0 +1,30 @@
+package aud.dummies.benchmarks.p01basics.c01algorithms;
+
+import ad.dummies.p01basics.c01algorithms.E01Summation;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Fork(1) // execute each benchmark on it's own JVM
+@Warmup(iterations = 3, time = 10, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 5, time = 10, timeUnit = TimeUnit.MILLISECONDS)
+public class E01SummationBenchmark {
+
+    @State(Scope.Thread)
+    public static class SumSetup {
+        @Param({"10", "100", "1000", "10000", "100000"})
+        private int upTo;
+    }
+
+    @Benchmark
+    public int benchmarkSummation1(SumSetup state) {
+        return E01Summation.summation1(state.upTo);
+    }
+
+    @Benchmark
+    public int benchmarkSummation2(SumSetup state) {
+        return E01Summation.summation2(state.upTo);
+    }
+}
