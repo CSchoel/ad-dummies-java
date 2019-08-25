9f7ffe2766d9e5e20fc6317ff29819cb06b770b7
Christopher Schölzel <christopher.schoelzel@gmx.net>
2019-08-25 22:43:56 +0200

    refactors riddle and weight tests

--- src/test/java/ad/dummies/p01basics/c01algorithms/E03RiddleTest.java
+++ src/test/java/ad/dummies/p01basics/c01algorithms/E03RiddleTest.java
@@ -1,38 +1,30 @@
 package ad.dummies.p01basics.c01algorithms;
 
 import org.junit.jupiter.api.Test;
-
-import java.util.Random;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class E03RiddleTest {
+    @CsvSource({"1,1,1", "1,5,5", "5,1,5", "2,7,14", "6,7,42", "32,32,1024"})
+    @ParameterizedTest(name="riddle({0},{1})")
+    public void riddle(int n, int m, int expected) {
+        assertEquals( expected , E03Riddle.riddle(n, m));
+    }
+
     @Test
-    public void riddle() {
-        assertEquals( 1 , E03Riddle.riddle(1, 1));
-        assertEquals( 5 , E03Riddle.riddle(1, 5));
-        assertEquals( 5 , E03Riddle.riddle(5, 1));
-        assertEquals( 0 , E03Riddle.riddle(0, 7));
-        assertEquals( 0 , E03Riddle.riddle(7, 0));
-        assertEquals(14, E03Riddle.riddle(2, 7));
-        assertEquals(42, E03Riddle.riddle(6, 7));
-        assertEquals(1024, E03Riddle.riddle(32, 32));
-        assertEquals(
-                3 * 7 * 7 * 11 * 11 * 13 * 13 * 15,
-                E03Riddle.riddle(3 * 7 * 7 * 11, 11 * 13 * 13 * 15)
-        );
+    public void riddleLargeInput() {
         assertEquals(1 << 31, E03Riddle.riddle(1 << 15, 1 << 16));
     }
 
     @Test
-    public void riddleLoop() {
-        Random r = new Random();
-        for(int i = 0; i < 1000; ++i) {
-            int x = r.nextInt(Short.MAX_VALUE);
-            int y = r.nextInt(Short.MAX_VALUE);
-            int res = E03Riddle.riddle(x, y);
-            String msg = String.format("%d * %d != %d", x, y, res);
-            assertEquals(x * y, res, msg);
-        }
+    public void riddleNzero() {
+        assertEquals(0, E03Riddle.riddle(0, 7));
+    }
+
+    @Test
+    public void riddleMzero() {
+        assertEquals(0, E03Riddle.riddle(7, 0));
     }
 }
--- src/test/java/ad/dummies/p01basics/c02quality/E01WeightTest.java
+++ src/test/java/ad/dummies/p01basics/c02quality/E01WeightTest.java
@@ -2,25 +2,119 @@ package ad.dummies.p01basics.c02quality;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.function.Function;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class E01WeightTest {
-    public void minWeight(Function<double[], Integer> max) {
-        assertEquals(0, max.apply(new double[]{90}));
-        assertEquals(1, max.apply(new double[]{94.5, 90}));
-        assertEquals(0, max.apply(new double[]{90, 94.5}));
-        assertEquals(0, max.apply(new double[]{90, 94.5}));
+
+    @Test
+    public void minWeight1OneElement() {
+        assertEquals(0, E01Weight.minWeight1(new double[]{90}));
     }
 
     @Test
-    public void minWeight1() {
-        minWeight(E01Weight::minWeight1);
+    public void minWeight1FirstOfTwo() {
+        assertEquals(0, E01Weight.minWeight1(new double[]{90, 94.5}));
     }
 
     @Test
-    public void minWeight2() {
-        minWeight(E01Weight::minWeight2);
+    public void minWeight1SecondOfTwo() {
+        assertEquals(1, E01Weight.minWeight1(new double[]{94.5, 90}));
+    }
+
+    @Test
+    public void minWeight1Ascending() {
+        assertEquals(0, E01Weight.minWeight1(new double[]{
+                70, 75, 80, 85, 90, 95
+        }));
+    }
+
+    @Test
+    public void minWeight1Descending() {
+        assertEquals(5, E01Weight.minWeight1(new double[]{
+                95, 90, 85, 80, 75, 70
+        }));
+    }
+
+    @Test
+    public void minWeight1Unordered() {
+        assertEquals(3, E01Weight.minWeight1(new double[]{
+                95, 75, 85, 70, 80, 90
+        }));
+    }
+
+    @Test
+    public void minWeight1MinValueExistsTwice() {
+        double[] people = {
+                95, 70, 85, 70, 80, 90
+        };
+        int res = E01Weight.minWeight1(people);
+        // two possible positions => only check value at position
+        assertEquals(70, people[res]);
+    }
+
+    @Test
+    public void minWeight1AllEqual() {
+        double[] people = {
+                80, 80, 80, 80, 80, 80
+        };
+        int res = E01Weight.minWeight1(people);
+        // six possible positions => only check value at position
+        assertEquals(80, people[res]);
+    }
+
+    @Test
+    public void minWeight2OneElement() {
+        assertEquals(0, E01Weight.minWeight2(new double[]{90}));
+    }
+
+    @Test
+    public void minWeight2FirstOfTwo() {
+        assertEquals(0, E01Weight.minWeight2(new double[]{90, 94.5}));
+    }
+
+    @Test
+    public void minWeight2SecondOfTwo() {
+        assertEquals(1, E01Weight.minWeight2(new double[]{94.5, 90}));
+    }
+
+    @Test
+    public void minWeight2Ascending() {
+        assertEquals(0, E01Weight.minWeight2(new double[]{
+                70, 75, 80, 85, 90, 95
+        }));
+    }
+
+    @Test
+    public void minWeight2Descending() {
+        assertEquals(5, E01Weight.minWeight2(new double[]{
+                95, 90, 85, 80, 75, 70
+        }));
+    }
+
+    @Test
+    public void minWeight2Unordered() {
+        assertEquals(3, E01Weight.minWeight2(new double[]{
+                95, 75, 85, 70, 80, 90
+        }));
+    }
+
+    @Test
+    public void minWeight2MinValueExistsTwice() {
+        double[] people = {
+                95, 70, 85, 70, 80, 90
+        };
+        int res = E01Weight.minWeight2(people);
+        // two possible positions => only check value at position
+        assertEquals(70, people[res]);
+    }
+
+    @Test
+    public void minWeight2AllEqual() {
+        double[] people = {
+                80, 80, 80, 80, 80, 80
+        };
+        int res = E01Weight.minWeight2(people);
+        // six possible positions => only check value at position
+        assertEquals(80, people[res]);
     }
 }
