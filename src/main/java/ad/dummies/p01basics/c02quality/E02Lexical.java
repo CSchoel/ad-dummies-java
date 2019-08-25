b636df08fab18152be1e68ecf840ea0a0991e3d2
Christopher Schölzel <christopher.schoelzel@gmx.net>
2019-08-25 23:04:54 +0200

    bugfix: fixes start value of lexicalFirst

--- src/main/java/ad/dummies/p01basics/c02quality/E02Lexical.java
+++ src/main/java/ad/dummies/p01basics/c02quality/E02Lexical.java
@@ -2,7 +2,8 @@ package ad.dummies.p01basics.c02quality;
 
 public class E02Lexical {
     public static String lexicalFirst(String[] words) {
-        String f = "";
+        assert words.length > 0;
+        String f = words[0];
         for (String s : words) {
             if (lexicalLess(s, f)) {
                 f = s;
