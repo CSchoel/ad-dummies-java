b8133eec5cbfc26317ae58ec4205b4f4ef529075
Christopher Schölzel <christopher.schoelzel@gmx.net>
2019-08-25 18:59:51 +0200

    refactors GGT test

--- src/test/java/ad/dummies/p01basics/c01algorithms/E02GGTTest.java
+++ src/test/java/ad/dummies/p01basics/c01algorithms/E02GGTTest.java
@@ -1,32 +1,23 @@
 package ad.dummies.p01basics.c01algorithms;
 
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.opentest4j.AssertionFailedError;
 
 import java.math.BigInteger;
 import java.time.Duration;
-import java.util.function.BiFunction;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.Alphanumeric.class)
 public class E02GGTTest {
-    public void ggt(BiFunction<Integer, Integer, Integer> ggt) {
-        assertEquals(0, ggt.apply(0, 0));
-        assertEquals(4, ggt.apply(0, 4));
-        assertEquals(4, ggt.apply(4, 0));
-        assertEquals(1, ggt.apply(1, 0));
-        assertEquals(1, ggt.apply(0, 1));
-        assertEquals(2, ggt.apply(4, 2));
-        assertEquals(3, ggt.apply(3 * 7 * 13, 3 * 3 * 11));
-        assertEquals(55, ggt.apply(5 * 11 * 23, 5 * 11 * 2 * 2));
-        assertEquals(1, ggt.apply(3 * 7 * 11, 5 * 13 * 17));
-        assertEquals(1024, ggt.apply(1024, 1024 * 13));
-        assertEquals(1, ggt.apply((int) Short.MAX_VALUE, Short.MAX_VALUE - 1));
-    }
 
     @Test
-    public void ggt1() {
+    public void ggt1shouldNotTerminate() {
         // assert that ggt1(6, 15) does not complete within 1 ms
         Executable timeout = () -> assertTimeoutPreemptively(Duration.ofMillis(1), () -> {
             E02GGT.ggt1(BigInteger.valueOf(6), BigInteger.valueOf(15));
@@ -34,18 +25,81 @@ public class E02GGTTest {
         assertThrows(AssertionFailedError.class, timeout);
     }
 
-    @Test
-    public void ggt2a() {
-        ggt(E02GGT::ggt2a);
+    @CsvSource({"0,0,0", "0,4,4", "4,0,4", "1,0,1", "0,1,1"})
+    @ParameterizedTest(name = "ggt({0},{1})")
+    public void ggt2aZeroInputs(int n, int m, int expected) {
+        assertEquals(expected, E02GGT.ggt2a(n, m));
     }
 
     @Test
-    public void ggt2b() {
-        ggt(E02GGT::ggt2b);
+    public void ggt2aNoCommonPrimeFactor() {
+        assertEquals(1, E02GGT.ggt2a(3 * 7 * 11, 5 * 13 * 17));
     }
 
     @Test
-    public void ggt3() {
-        ggt(E02GGT::ggt3);
+    public void ggt2aOneCommonPrimeFactor() {
+        assertEquals(3, E02GGT.ggt2a(3 * 7 * 13, 3 * 3 * 11));
+    }
+
+    @Test
+    public void ggt2aTwoCommonPrimeFactors() {
+        assertEquals(55, E02GGT.ggt2a(5 * 11 * 23, 5 * 11 * 2 * 2));
+    }
+
+    @Test
+    public void ggt2aSameCommonPrimeFactorMultipleTimes() {
+        assertEquals(1024, E02GGT.ggt2a(1024, 1024 * 13));
+    }
+
+    @CsvSource({"0,0,0", "0,4,4", "4,0,4", "1,0,1", "0,1,1"})
+    @ParameterizedTest(name = "ggt({0},{1})")
+    public void ggt2bZeroInputs(int n, int m, int expected) {
+        assertEquals(expected, E02GGT.ggt2b(n, m));
+    }
+
+    @Test
+    public void ggt2bNoCommonPrimeFactor() {
+        assertEquals(1, E02GGT.ggt2b(3 * 7 * 11, 5 * 13 * 17));
+    }
+
+    @Test
+    public void ggt2bOneCommonPrimeFactor() {
+        assertEquals(3, E02GGT.ggt2b(3 * 7 * 13, 3 * 3 * 11));
+    }
+
+    @Test
+    public void ggt2bTwoCommonPrimeFactors() {
+        assertEquals(55, E02GGT.ggt2b(5 * 11 * 23, 5 * 11 * 2 * 2));
+    }
+
+    @Test
+    public void ggt2bSameCommonPrimeFactorMultipleTimes() {
+        assertEquals(1024, E02GGT.ggt2b(1024, 1024 * 13));
+    }
+
+    @CsvSource({"0,0,0", "0,4,4", "4,0,4", "1,0,1", "0,1,1"})
+    @ParameterizedTest(name = "ggt({0},{1})")
+    public void ggt3ZeroInputs(int n, int m, int expected) {
+        assertEquals(expected, E02GGT.ggt3(n, m));
+    }
+
+    @Test
+    public void ggt3NoCommonPrimeFactor() {
+        assertEquals(1, E02GGT.ggt3(3 * 7 * 11, 5 * 13 * 17));
+    }
+
+    @Test
+    public void ggt3OneCommonPrimeFactor() {
+        assertEquals(3, E02GGT.ggt3(3 * 7 * 13, 3 * 3 * 11));
+    }
+
+    @Test
+    public void ggt3TwoCommonPrimeFactors() {
+        assertEquals(55, E02GGT.ggt3(5 * 11 * 23, 5 * 11 * 2 * 2));
+    }
+
+    @Test
+    public void ggt3SameCommonPrimeFactorMultipleTimes() {
+        assertEquals(1024, E02GGT.ggt3(1024, 1024 * 13));
     }
 }
