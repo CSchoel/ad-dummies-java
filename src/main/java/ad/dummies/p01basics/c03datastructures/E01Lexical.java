81d1ec553aff023d88d483a9944eca2912c94fec
Christopher Schölzel <christopher.schoelzel@gmx.net>
2019-09-09 23:11:28 +0200

    adds second instance of LexicalFirst example from chapter 3

---
+++ src/main/java/ad/dummies/p01basics/c03datastructures/E01Lexical.java
@@ -0,0 +1,37 @@
+package ad.dummies.p01basics.c03datastructures;
+
+public class E01Lexical {
+    public static String lexicalFirst(String[] words) {
+        assert words.length > 0;
+        // FIXME: start value of f would need to change if we consider lengths
+        // because then "" would be found as the lexically first word
+        String f = "";
+        for (String s : words) {
+            if (before(s, f)) {
+                f = s;
+            }
+        }
+        return f;
+    }
+
+    private static boolean before(String s1, String s2) {
+        // FIXME: book forgot to increment counter
+        for (int i = 0; i < Math.min(s1.length(), s2.length()); i++) {
+            if (s1.charAt(i) > s2.charAt(i)) {
+                return false;
+            }
+            // FIXME: book version does not include "else if" part
+            // This means it will fail for values of s1 that should return
+            // true due to the first non-matching character, but return false
+            // due to a further following character mismatch (e.g.
+            // before("ab","ba") will return false instead of true).
+            // You can test this behavior by commenting the following three
+            // lines.
+            else if (s1.charAt(i) < s2.charAt(i)) {
+                return true;
+            }
+        }
+        // FIXME: book version does not consider sequences of different lengths
+        return true;
+    }
+}
---
+++ src/test/java/ad/dummies/p01basics/c03datastructures/E01LexicalTest.java
@@ -0,0 +1,84 @@
+package ad.dummies.p01basics.c03datastructures;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestMethodOrder(MethodOrderer.Alphanumeric.class)
+public class E01LexicalTest {
+    @Test
+    void lexicalFirstOneElement() {
+        assertEquals("alpha", E01Lexical.lexicalFirst(new String[]{
+                "alpha"
+        }));
+
+    }
+
+    @Test
+    void lexicalFirstFirstOfTwo() {
+        assertEquals("alpha", E01Lexical.lexicalFirst(new String[]{
+                "alpha", "beta"
+        }));
+
+    }
+
+    @Test
+    void lexicalFirstSecondOfTwo() {
+        assertEquals("alpha", E01Lexical.lexicalFirst(new String[]{
+                "beta", "alpha"
+        }));
+    }
+
+    @Test
+    void lexicalFirstSecondOfTwoBySecondChar() {
+        assertEquals("aa", E01Lexical.lexicalFirst(new String[]{
+                "ab", "aa"
+        }));
+    }
+
+    @Disabled("Disabled because book does not consider different lengths")
+    @Test
+    void lexicalFirstSecondOfTwoByLength() {
+        assertEquals("aa", E01Lexical.lexicalFirst(new String[]{
+                "aa", "aaa"
+        }));
+    }
+
+    @Test
+    void lexicalFirstAscending() {
+        assertEquals("aa", E01Lexical.lexicalFirst(new String[]{
+                "aa", "ab", "ac", "ba", "bb", "bc", "ca", "cb", "cc"
+        }));
+    }
+
+    @Test
+    void lexicalFirstDescending() {
+        assertEquals("aa", E01Lexical.lexicalFirst(new String[]{
+                "cc", "cb", "ca", "bc", "bb", "ba", "ac", "ab", "aa"
+        }));
+    }
+
+    @Test
+    void lexicalFirstUnordered() {
+        assertEquals("aa", E01Lexical.lexicalFirst(new String[]{
+                "cb", "bb", "ab", "ca", "cc", "bc", "aa", "ba", "ac"
+        }));
+    }
+
+    @Test
+    void lexicalFirstFirstExistsTwice() {
+        assertEquals("aa", E01Lexical.lexicalFirst(new String[]{
+                "cb", "bb", "aa", "ca", "cc", "bc", "aa", "ba", "ac"
+        }));
+    }
+
+    @Test
+    void lexicalFirstAllEqual() {
+        assertEquals("bb", E01Lexical.lexicalFirst(new String[]{
+                "bb", "bb", "bb", "bb", "bb", "bb", "bb", "bb", "bb"
+        }));
+    }
+}
