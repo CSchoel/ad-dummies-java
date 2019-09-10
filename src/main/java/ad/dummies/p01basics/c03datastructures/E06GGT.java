1c1a4eafccc23edd52f52a165903e5425c2d4ee3
Christopher Schölzel <christopher.schoelzel@gmx.net>
2019-09-10 22:47:22 +0200

    adds tuple variant of ggt and adds tests

--- src/main/java/ad/dummies/p01basics/c03datastructures/E06GGT.java
+++ src/main/java/ad/dummies/p01basics/c03datastructures/E06GGT.java
@@ -2,22 +2,48 @@ package ad.dummies.p01basics.c03datastructures;
 
 public class E06GGT {
     public static int GGTRec(int m, int n) {
-        if (m == n) {
-            return n;
-        }
+        // FIXME: book assumes n > 0 and m > 0
+        if (m == 0) { return n; }
+        if (n == 0) { return m; }
+        if (m == n) { return n; }
         return GGTRec(Math.max(m, n) - Math.min(m, n), Math.min(m, n));
     }
 
     public static int GGTIter(int x, int y) {
+        // FIXME: book assumes n > 0 and m > 0
+        if (x == 0) { return y; }
+        if (y == 0) { return x; }
         while (x != y) {
             if (x > y) {
                 x = x - y;
             } else {
-                y = x - y;
+                y = y - x;
             }
         }
         return x;
     }
 
-    // tuple variant is not possible without creating own tuple type
+    public static class Tuple {
+        private int x;
+        private int y;
+        public Tuple(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        public int x() { return x; }
+        public int y() { return y; }
+        public int max() { return x > y ? x : y; }
+        public int min() { return x < y ? x : y; }
+    }
+
+    public static int GGTIterTuple(int x, int y) {
+        // FIXME: book assumes n > 0 and m > 0
+        if (x == 0) { return y; }
+        if (y == 0) { return x; }
+        Tuple args = new Tuple(x, y);
+        while (args.x() != args.y()) {
+            args = new Tuple(args.max() - args.min(), args.min());
+        }
+        return args.x();
+    }
 }
---
+++ src/test/java/ad/dummies/p01basics/c03datastructures/E06GGTTest.java
@@ -0,0 +1,92 @@
+package ad.dummies.p01basics.c03datastructures;
+
+import ad.dummies.p01basics.c01algorithms.E02GGT;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.Alphanumeric.class)
+class E06GGTTest {
+
+    @CsvSource({"0,0,0", "0,4,4", "4,0,4", "1,0,1", "0,1,1"})
+    @ParameterizedTest(name = "ggt({0},{1})")
+    public void GGTRecZeroInputs(int n, int m, int expected) {
+        assertEquals(expected, E06GGT.GGTRec(n, m));
+    }
+
+    @Test
+    public void GGTRecNoCommonPrimeFactor() {
+        assertEquals(1, E06GGT.GGTRec(3 * 7 * 11, 5 * 13 * 17));
+    }
+
+    @Test
+    public void GGTRecOneCommonPrimeFactor() {
+        assertEquals(3, E06GGT.GGTRec(3 * 7 * 13, 3 * 3 * 11));
+    }
+
+    @Test
+    public void GGTRecTwoCommonPrimeFactors() {
+        assertEquals(55, E06GGT.GGTRec(5 * 11 * 23, 5 * 11 * 2 * 2));
+    }
+
+    @Test
+    public void GGTRecSameCommonPrimeFactorMultipleTimes() {
+        assertEquals(1024, E06GGT.GGTRec(1024, 1024 * 13));
+    }
+
+    @CsvSource({"0,0,0", "0,4,4", "4,0,4", "1,0,1", "0,1,1"})
+    @ParameterizedTest(name = "ggt({0},{1})")
+    public void GGTIterZeroInputs(int n, int m, int expected) {
+        assertEquals(expected, E06GGT.GGTIter(n, m));
+    }
+
+    @Test
+    public void GGTIterNoCommonPrimeFactor() {
+        assertEquals(1, E06GGT.GGTIter(3 * 7 * 11, 5 * 13 * 17));
+    }
+
+    @Test
+    public void GGTIterOneCommonPrimeFactor() {
+        assertEquals(3, E06GGT.GGTIter(3 * 7 * 13, 3 * 3 * 11));
+    }
+
+    @Test
+    public void GGTIterTwoCommonPrimeFactors() {
+        assertEquals(55, E06GGT.GGTIter(5 * 11 * 23, 5 * 11 * 2 * 2));
+    }
+
+    @Test
+    public void GGTIterSameCommonPrimeFactorMultipleTimes() {
+        assertEquals(1024, E06GGT.GGTIter(1024, 1024 * 13));
+    }
+
+    @CsvSource({"0,0,0", "0,4,4", "4,0,4", "1,0,1", "0,1,1"})
+    @ParameterizedTest(name = "ggt({0},{1})")
+    public void GGTIterTupleZeroInputs(int n, int m, int expected) {
+        assertEquals(expected, E06GGT.GGTIterTuple(n, m));
+    }
+
+    @Test
+    public void GGTIterTupleNoCommonPrimeFactor() {
+        assertEquals(1, E06GGT.GGTIterTuple(3 * 7 * 11, 5 * 13 * 17));
+    }
+
+    @Test
+    public void GGTIterTupleOneCommonPrimeFactor() {
+        assertEquals(3, E06GGT.GGTIterTuple(3 * 7 * 13, 3 * 3 * 11));
+    }
+
+    @Test
+    public void GGTIterTupleTwoCommonPrimeFactors() {
+        assertEquals(55, E06GGT.GGTIterTuple(5 * 11 * 23, 5 * 11 * 2 * 2));
+    }
+
+    @Test
+    public void GGTIterTupleSameCommonPrimeFactorMultipleTimes() {
+        assertEquals(1024, E06GGT.GGTIterTuple(1024, 1024 * 13));
+    }
+}
