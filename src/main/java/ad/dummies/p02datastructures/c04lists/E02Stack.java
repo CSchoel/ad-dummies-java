eaaa7fcccb9a982aa0beada5fa8050866417df95
Christopher Schölzel <christopher.schoelzel@gmx.net>
2019-09-22 22:27:24 +0200

    adds stack example

---
+++ src/main/java/ad/dummies/p02datastructures/c04lists/E02Stack.java
@@ -0,0 +1,118 @@
+package ad.dummies.p02datastructures.c04lists;
+
+public class E02Stack {
+    public interface Stack<E> {
+        void push(E el);
+        E pop();
+    }
+
+    public static class Stack01Linked<E> implements Stack<E> {
+        Cell<E> head;
+
+        @Override
+        public void push(E el) {
+            head = new Cell<>(el, head);
+        }
+
+        @Override
+        public E pop() {
+            assert head != null; // stack must not be empty
+            E res = head.element;
+            head = head.next;
+            return res;
+        }
+
+        private static class Cell<E> {
+            E element;
+            Cell<E> next;
+            public Cell(E element, Cell<E> next) {
+                this.element = element;
+                this.next = next;
+            }
+        }
+    }
+
+    public static class Stack02Array<E> implements Stack<E> {
+        private E[] a;
+        int top;
+        public Stack02Array(int n) {
+            // (E[]) here is a small hack that is required because Java
+            // eliminates type parameter information during compilation.
+            // Essentially our class believes that a is of type E[], but
+            // at runtime it will be of type Object[]. This is fine as long
+            // as we do not expose ar outside of this class.
+            a = (E[]) new Object[n];
+            top = 0;
+        }
+
+        @Override
+        public void push(E el) {
+            assert top < a.length; // stack must not be full
+            a[top] = el;
+            top++;
+        }
+
+        @Override
+        public E pop() {
+            assert top > 0; // stack must not be empty
+            top--;
+            return a[top];
+        }
+    }
+
+    public static class Stack03AlgDT<E> implements Stack<E> {
+        private interface TList<E> {}
+        private static class Nil<E> implements TList<E> {}
+        private static class Cons<E> implements TList<E> {
+            private E head;
+            private TList<E> tail;
+            public Cons(E head, TList<E> tail) {
+                this.head = head;
+                this.tail = tail;
+            }
+        }
+
+        public Stack03AlgDT() {}
+        private Stack03AlgDT(TList<E> stackRep) {
+            this.stackRep = stackRep;
+        }
+
+        TList<E> stackRep = new Nil<>();
+
+        @Override
+        public void push(E el) {
+            stackRep = new Cons<>(el, stackRep);
+        }
+
+        @Override
+        public E pop() {
+            assert stackRep instanceof Cons; // stack must not be empty
+            Cons<E> stackRepCons = (Cons<E>) stackRep;
+            E res = stackRepCons.head;
+            stackRep = stackRepCons.tail;
+            return res;
+        }
+
+        // helper class required to emulate two return values
+        public static class ElementAndStack03AlgDT<E> {
+            public E element;
+            public Stack03AlgDT<E> stack;
+            public ElementAndStack03AlgDT(E element, Stack03AlgDT<E> stack) {
+                this.element = element;
+                this.stack = stack;
+            }
+        }
+
+        public static <E> Stack03AlgDT<E> pushA(E el, Stack03AlgDT<E> stack) {
+            return new Stack03AlgDT<>(new Cons<>(el, stack.stackRep));
+        }
+        public static <E> ElementAndStack03AlgDT<E> popA(Stack03AlgDT<E> stack) {
+            assert stack.stackRep instanceof Cons;
+            Cons<E> stackRepCons = (Cons<E>) stack.stackRep;
+            return new ElementAndStack03AlgDT<>(
+                    stackRepCons.head,
+                    new Stack03AlgDT<>(stackRepCons.tail)
+            );
+        }
+    }
+}
---
+++ src/test/java/ad/dummies/p02datastructures/c04lists/E02StackTest.java
@@ -0,0 +1,274 @@
+package ad.dummies.p02datastructures.c04lists;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static ad.dummies.p02datastructures.c04lists.E02Stack.*;
+
+@TestMethodOrder(MethodOrderer.Alphanumeric.class)
+class E02StackTest {
+
+    @Test
+    public void stack01PushAndPopYieldsPushedElement() {
+        Stack<Integer> s = new Stack01Linked<>();
+        s.push(9);
+        assertEquals(9, s.pop());
+    }
+
+    @Test
+    public void stack01TwoPushesAndTwoPopsYieldElementsInReversedOrder() {
+        Stack<Integer> s = new Stack01Linked<>();
+        s.push(9);
+        s.push(-1);
+        assertEquals(-1, s.pop());
+        assertEquals(9, s.pop());
+    }
+
+    @Test
+    public void stack01FivePushesAndFivePopsYieldElementsInReversedOrder() {
+        Stack<Integer> s = new Stack01Linked<>();
+        s.push(9);
+        s.push(-1);
+        s.push(8);
+        s.push(0);
+        s.push(1);
+        assertEquals(1, s.pop());
+        assertEquals(0, s.pop());
+        assertEquals(8, s.pop());
+        assertEquals(-1, s.pop());
+        assertEquals(9, s.pop());
+    }
+
+    @Test
+    public void stack01PopsBetweenPushesYieldCorrectElements() {
+        Stack<Integer> s = new Stack01Linked<>();
+        s.push(9);
+        s.push(-1);
+        s.push(8);
+        assertEquals(8, s.pop());
+        s.push(0);
+        s.push(1);
+        assertEquals(1, s.pop());
+
+        assertEquals(0, s.pop());
+        assertEquals(-1, s.pop());
+        assertEquals(9, s.pop());
+    }
+
+    @Test
+    public void stack02withCapacityOneAcceptsOnePush() {
+        Stack<Integer> s = new Stack02Array<>(1);
+        s.push(9);
+        assertEquals(9, s.pop());
+    }
+
+    @Test
+    public void stack02PushAndPopYieldsPushedElement() {
+        Stack<Integer> s = new Stack02Array<>(10);
+        s.push(9);
+        assertEquals(9, s.pop());
+    }
+
+    @Test
+    public void stack02TwoPushesAndTwoPopsYieldElementsInReversedOrder() {
+        Stack<Integer> s = new Stack02Array<>(10);
+        s.push(9);
+        s.push(-1);
+        assertEquals(-1, s.pop());
+        assertEquals(9, s.pop());
+    }
+
+    @Test
+    public void stack02FivePushesAndFivePopsYieldElementsInReversedOrder() {
+        Stack<Integer> s = new Stack02Array<>(10);
+        s.push(9);
+        s.push(-1);
+        s.push(8);
+        s.push(0);
+        s.push(1);
+        assertEquals(1, s.pop());
+        assertEquals(0, s.pop());
+        assertEquals(8, s.pop());
+        assertEquals(-1, s.pop());
+        assertEquals(9, s.pop());
+    }
+
+    @Test
+    public void stack02PopsBetweenPushesYieldCorrectElements() {
+        Stack<Integer> s = new Stack02Array<>(10);
+        s.push(9);
+        s.push(-1);
+        s.push(8);
+        assertEquals(8, s.pop());
+        s.push(0);
+        s.push(1);
+        assertEquals(1, s.pop());
+
+        assertEquals(0, s.pop());
+        assertEquals(-1, s.pop());
+        assertEquals(9, s.pop());
+    }
+
+    @Test
+    public void stack03PushAndPopYieldsPushedElement() {
+        Stack<Integer> s = new Stack03AlgDT<>();
+        s.push(9);
+        assertEquals(9, s.pop());
+    }
+
+    @Test
+    public void stack03TwoPushesAndTwoPopsYieldElementsInReversedOrder() {
+        Stack<Integer> s = new Stack03AlgDT<>();
+        s.push(9);
+        s.push(-1);
+        assertEquals(-1, s.pop());
+        assertEquals(9, s.pop());
+    }
+
+    @Test
+    public void stack03FivePushesAndFivePopsYieldElementsInReversedOrder() {
+        Stack<Integer> s = new Stack03AlgDT<>();
+        s.push(9);
+        s.push(-1);
+        s.push(8);
+        s.push(0);
+        s.push(1);
+        assertEquals(1, s.pop());
+        assertEquals(0, s.pop());
+        assertEquals(8, s.pop());
+        assertEquals(-1, s.pop());
+        assertEquals(9, s.pop());
+    }
+
+    @Test
+    public void stack03PopsBetweenPushesYieldCorrectElements() {
+        Stack<Integer> s = new Stack03AlgDT<>();
+        s.push(9);
+        s.push(-1);
+        s.push(8);
+        assertEquals(8, s.pop());
+        s.push(0);
+        s.push(1);
+        assertEquals(1, s.pop());
+
+        assertEquals(0, s.pop());
+        assertEquals(-1, s.pop());
+        assertEquals(9, s.pop());
+    }
+
+    @Test
+    public void stack03APushAndPopYieldsPushedElement() {
+        Stack03AlgDT<Integer> s = new Stack03AlgDT<>();
+        Stack03AlgDT.ElementAndStack03AlgDT popRes;
+        s = Stack03AlgDT.pushA(9, s);
+        popRes = Stack03AlgDT.popA(s);
+        assertEquals(9, popRes.element);
+    }
+
+    @Test
+    public void stack03APushADoesNotAffectOriginalStack() {
+        Stack03AlgDT<Integer> s = new Stack03AlgDT<>();
+        s.push(4);
+        Stack03AlgDT.pushA(9, s);
+        assertEquals(4, s.pop());
+    }
+
+    @Test
+    public void stack03APopADoesNotAffectOriginalStack() {
+        Stack03AlgDT<Integer> s = new Stack03AlgDT<>();
+        s.push(4);
+        Stack03AlgDT.ElementAndStack03AlgDT popRes = Stack03AlgDT.popA(s);
+        assertEquals(4, s.pop());
+    }
+
+    @Test
+    public void stack03APushAResultIsNotAffectedByPushesToOriginalStack() {
+        Stack03AlgDT<Integer> s = new Stack03AlgDT<>();
+        s.push(4);
+        Stack03AlgDT<Integer> s2 = Stack03AlgDT.pushA(9, s);
+        s.push(9);
+        s.push(-1);
+        assertEquals(9, Stack03AlgDT.popA(s2).element);
+    }
+
+    @Test
+    public void stack03APopAResultIsNotAffectedByPushesToOriginalStack() {
+        Stack03AlgDT<Integer> s = new Stack03AlgDT<>();
+        Stack03AlgDT.ElementAndStack03AlgDT<Integer> popRes;
+        Stack03AlgDT<Integer> s2 = Stack03AlgDT.pushA(9, s);
+        s.push(4);
+        s.push(0);
+        popRes = Stack03AlgDT.popA(s2);
+        assertEquals(9, popRes.element);
+    }
+
+    @Test
+    public void stack03ATwoPushesAndTwoPopsYieldElementsInReversedOrder() {
+        Stack03AlgDT<Integer> s = new Stack03AlgDT<>();
+        Stack03AlgDT.ElementAndStack03AlgDT<Integer> popRes;
+        s = Stack03AlgDT.pushA(9, s);
+        s = Stack03AlgDT.pushA(-1, s);
+        popRes = Stack03AlgDT.popA(s);
+        s = popRes.stack;
+        assertEquals(-1, popRes.element);
+        popRes = Stack03AlgDT.popA(s);
+        s = popRes.stack;
+        assertEquals(9, popRes.element);
+    }
+
+    @Test
+    public void stack03AFivePushesAndFivePopsYieldElementsInReversedOrder() {
+        Stack03AlgDT<Integer> s = new Stack03AlgDT<>();
+        Stack03AlgDT.ElementAndStack03AlgDT<Integer> popRes;
+        s = Stack03AlgDT.pushA(9, s);
+        s = Stack03AlgDT.pushA(-1, s);
+        s = Stack03AlgDT.pushA(8, s);
+        s = Stack03AlgDT.pushA(0, s);
+        s = Stack03AlgDT.pushA(1, s);
+        popRes = Stack03AlgDT.popA(s);
+        s = popRes.stack;
+        assertEquals(1, popRes.element);
+        popRes = Stack03AlgDT.popA(s);
+        s = popRes.stack;
+        assertEquals(0, popRes.element);
+        popRes = Stack03AlgDT.popA(s);
+        s = popRes.stack;
+        assertEquals(8, popRes.element);
+        popRes = Stack03AlgDT.popA(s);
+        s = popRes.stack;
+        assertEquals(-1, popRes.element);
+        popRes = Stack03AlgDT.popA(s);
+        s = popRes.stack;
+        assertEquals(9, popRes.element);
+    }
+
+    @Test
+    public void stack03APopsBetweenPushesYieldCorrectElements() {
+        Stack03AlgDT<Integer> s = new Stack03AlgDT<>();
+        Stack03AlgDT.ElementAndStack03AlgDT<Integer> popRes;
+        s = Stack03AlgDT.pushA(9, s);
+        s = Stack03AlgDT.pushA(-1, s);
+        s = Stack03AlgDT.pushA(8, s);
+        popRes = Stack03AlgDT.popA(s);
+        s = popRes.stack;
+        assertEquals(8, popRes.element);
+        s = Stack03AlgDT.pushA(0, s);
+        s = Stack03AlgDT.pushA(1, s);
+        popRes = Stack03AlgDT.popA(s);
+        s = popRes.stack;
+        assertEquals(1, popRes.element);
+
+        popRes = Stack03AlgDT.popA(s);
+        s = popRes.stack;
+        assertEquals(0, popRes.element);
+        popRes = Stack03AlgDT.popA(s);
+        s = popRes.stack;
+        assertEquals(-1, popRes.element);
+        popRes = Stack03AlgDT.popA(s);
+        s = popRes.stack;
+        assertEquals(9, popRes.element);
+    }
+}
