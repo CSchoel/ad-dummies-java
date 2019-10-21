86f82f0311bef036d9ccc9017b41c240fdefeab8
Christopher Schölzel <christopher.schoelzel@gmx.net>
2019-10-21 20:45:10 +0200

    adds test stub and benchmark for example 3.7.3

---
+++ src/jmh/java/ad/dummies/p03problems/c07sorting/E03QuickSortBenchmark.java
@@ -0,0 +1,47 @@
+package ad.dummies.p03problems.c07sorting;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * <p>JMH benchmarks for an example from the german book "Algorithms and data
+ * structures for dummies":</p>
+ *
+ * <p>A. Gogol-Döring and T. Letschert, <i>Algorithmen und Datenstrukturen für
+ * Dummies</i>. Weinheim, Germany: Wiley-VCH, 2019.</p>
+ *
+ * <p>The current version of these examples with unit tests and benchmarks can
+ * be found <a href="https://github.com/CSchoel/ad-dummies-java">on GitHub</a>.
+ * </p>
+ *
+ * @author Christopher Schölzel
+ * @see E03QuickSort
+ */
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Fork(1) // execute each benchmark on it's own JVM
+@Warmup(iterations = 2, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+public class E03QuickSortBenchmark {
+    @State(Scope.Thread)
+    public static class SortSetup {
+        @Param({"10", "100", "1000", "10000"})
+        private int length;
+        private Integer[] data;
+
+        @Setup
+        public void setup() {
+            data = new Random(667)
+                    .ints(length, -length/10, length/10)
+                    .boxed().toArray(Integer[]::new);
+        }
+    }
+
+    @Benchmark
+    public Integer[] mergeSortWorstRandom(SortSetup state) {
+        E03QuickSort.quickSort(state.data);
+        return state.data;
+    }
+}
