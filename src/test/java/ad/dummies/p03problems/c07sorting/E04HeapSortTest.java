3eae520fa306fd66663c66cfad3704dbb39fcfca
Christopher Schölzel <christopher.schoelzel@gmx.net>
2019-10-22 23:57:19 +0200

    adds rudimentary test

---
+++ src/test/java/ad/dummies/p03problems/c07sorting/E04HeapSortTest.java
@@ -0,0 +1,30 @@
+package ad.dummies.p03problems.c07sorting;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static ad.dummies.p03problems.c07sorting.E04HeapSort.*;
+
+/**
+ * <p>Unit tests for an example from the german book "Algorithms and data
+ * structures for dummies":</p>
+ *
+ * <p>A. Gogol-Döring and T. Letschert, <i>Algorithmen und Datenstrukturen für
+ * Dummies</i>. Weinheim, Germany: Wiley-VCH, 2019.</p>
+ *
+ * <p>The current version of these examples with unit tests and benchmarks can
+ * be found <a href="https://github.com/CSchoel/ad-dummies-java">on GitHub</a>.
+ * </p>
+ *
+ * @author Christopher Schölzel
+ * @see E04HeapSort
+ */
+class E04HeapSortTest {
+
+    //TODO implement actual unit tests
+
+    @Test
+    public void mainMethodDoesNotThrowAnyExceptions() {
+        main(new String[0]);
+    }
+}
